d3cf7490ba681111a0aab478f8e7f9d980a838f1
Addai Christopher <addaichristopher1@gmail.com>
2023-10-31 20:59:29 -0700

    alpha

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+print("%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s" %
+      ('a','b','c','d','f','g','h','i','j','k','l','m','n','o','p','r','s','t','u','v','w','x','y','z'))
