4f4e7f7a0a9bdd4f00550f8463f0287dcc920e56
Addai Christopher <addaichristopher1@gmail.com>
2023-11-01 18:06:04 -0700

    resolve negative number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+neg = number < 0
 n = int(str(number)[-1])
+if neg:
+    n = 0 - n
 if n == 0:
     print(f"Last digit of {number} is {n} and is 0")
 elif n  < 6:
