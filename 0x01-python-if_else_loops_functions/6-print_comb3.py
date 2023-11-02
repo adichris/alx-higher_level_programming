c9df9638d3035bd085072c8df3aaf228a9f9260e
Addai Christopher <addaichristopher1@gmail.com>
2023-11-01 18:56:57 -0700

    researched

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 for i in range(10):
-    for j in range(i+1, 10):
-        print(f"{i}{j}", end=", ")
-print()
+    for j in range(10):
+        if j >= i and i != j:
+            if i == 8 and j == 9:
+                print(89)
+            else:
+                print("{}".format(str(i) + str(j)), end=", ")
