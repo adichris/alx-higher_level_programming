57dd39b0874606ff1f5ffbb725bfe17e8f0230f8
Addai Christopher <addaichristopher1@gmail.com>
2023-11-23 02:50:48 +0000

    fix bug

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
             i += 1
             print(x, sep="", end="")
     except ValueError:
-        return
+        i += 1
     else:
         print()
         return i
